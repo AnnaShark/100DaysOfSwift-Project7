58726a98485bfe5fe0eb03e8084f48022c968e16
Anna Shark <a.a.sharkova@gmail.com>
2022-09-06 09:58:54 +0200

    Add another tab with top rated petitions, petitions details veiw adjusted to the device size

--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -13,15 +13,30 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let urlString: String
         
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
-        //"http://api. whitehouse.gov/v1/petitions.jsons?limit=100"
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+            //"https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json" //"https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+
+        }
+
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
+                return
             }
         }
+        showError()
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed, please check your connection and try again", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     func parse(json: Data) {
@@ -45,6 +60,12 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
     }
     
 }
